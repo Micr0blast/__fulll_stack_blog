b9bd4a6416c4301cc2dd8a34151dab0a1c9ba9be
Micr0blast <ploetz_stefan@web.de>
2025-01-02 18:29:59 +0100

    feat: added createPost component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import './App.css'
-import { Post } from './components/Post.jsx'
+import { CreatePost } from './components/CreatePost.jsx'
 
 export function App() {
-  const sample = {
-    title: 'Hello React',
-    contents: 'This is a blog post',
-    author: 'MEEEEEE',
-  }
-  return Post(sample)
+  return <CreatePost />
 }
---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,19 @@
+export function CreatePost() {
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <div>
+        <lable htmlFor='create-title'>Title: </lable>
+        <input type='text' name='create-title' id='create-title' />
+      </div>
+      <br />
+      <div>
+        <lable htmlFor='create-author'>Author: </lable>
+        <input type='text' name='create-author' id='create-author' />
+      </div>
+      <br />
+      <textarea />
+      <br />
+      <input type='submit' value='Create' />
+    </form>
+  )
+}
