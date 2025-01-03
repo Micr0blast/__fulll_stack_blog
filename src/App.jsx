8733d7765beb5fbb30449df9b435d885760d2b0e
Micr0blast <ploetz_stefan@web.de>
2025-01-03 10:56:49 +0100

    feat: added list component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import './App.css'
 import { PostFilter } from './components/PostFilter.jsx'
 import { CreatePost } from './components/CreatePost.jsx'
 import { PostSorting } from './components/PostSorting.jsx'
+import { PostList } from './components/PostList.jsx'
 
 export function App() {
+  const samplePosts = [
+    { title: 'One', author: 'Me', contents: 'Triachars is a rule' },
+    { title: 'Two' },
+    { title: 'Three', author: 'He', contents: 'Fear the old blood' },
+  ]
   return (
     <div style={{ padding: 8 }}>
       <CreatePost />
@@ -14,6 +20,7 @@ export function App() {
       <br />
       <PostSorting fields={['createdAt', 'updatedAt']} />
       <hr />
+      <PostList posts={samplePosts} />
     </div>
   )
 }
